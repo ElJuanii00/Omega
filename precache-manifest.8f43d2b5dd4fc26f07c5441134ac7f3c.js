807346d66d98300539f36c2dee5231bad5d0bebc
Juan Ponce <me.juanponce@gmail.com>
2020-10-12 13:05:43 -0500

    Updates

---
+++ precache-manifest.8f43d2b5dd4fc26f07c5441134ac7f3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0347019c7049548bd917cb902670a56e",
+    "url": "/Omega/index.html"
+  },
+  {
+    "revision": "f26780ec74b881a44ae9",
+    "url": "/Omega/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc053c79f8f523204d95",
+    "url": "/Omega/static/js/2.46460176.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Omega/static/js/2.46460176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26780ec74b881a44ae9",
+    "url": "/Omega/static/js/main.522aa802.chunk.js"
+  },
+  {
+    "revision": "fa4bba226ffcd4723fd1",
+    "url": "/Omega/static/js/runtime-main.90a98511.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Omega/static/media/logo.5d5d9eef.svg"
+  }
+]);
